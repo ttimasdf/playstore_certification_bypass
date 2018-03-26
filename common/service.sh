ee1b4ce5598b1dcea4ca38739f46f01f3d451a76
ttimasdf <ttimasdf@users.noreply.github.com>
2018-03-26 18:56:33 +0800

    Fix Note 4 not working

--- common/service.sh
+++ common/service.sh
@@ -10,13 +10,15 @@ BUILD_FGP=$(getprop "ro.build.fingerprint")
 
 case $BUILD_FGP in
     *OnePlus3*)
-        resetprop "ro.build.description" "OnePlus3-user 7.1.1 NMF26F 108 dev-keys"
+        resetprop ro.build.description "OnePlus3-user 7.1.1 NMF26F 108 dev-keys"
         resetprop ro.build.fingerprint "OnePlus/OnePlus3/OnePlus3:7.1.1/NMF26F/08081200:user/release-keys"
         resetprop ro.build.oemfingerprint "7.1.1/NMF26F/08081200:user/release-keys"
         ;;
     *trlteduoszh*)
         resetprop ro.build.fingerprint "samsung/trlteduoszh/trltechnzh:6.0.1/MMB29M/N9100ZHU1DQH2:user/release-keys"
+        resetprop ro.build.display.id "trlteduoszh-user 6.0.1 MMB29M N9100ZHU1DQH2 release-keys"
         resetprop ro.build.id "MMB29M"
+        resetprop ro.build.version.release "6.0.1"
         resetprop ro.build.version.incremental "N9100ZHU1DQH2"
         ;;
     *OnePlus2*)
